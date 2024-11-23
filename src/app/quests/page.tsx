a47ab22a4b600be0c94e108c2b06440c45c87b1d
Philip Burg <philipburgi@web.de>
2024-11-23 11:07:03 +0100

    created initial Quests page

---
+++ src/app/quests/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default  function QuestsPage() {
+    return (
+        <div>
+            <h1>Hier entstehen tasks</h1>
+            <p>och domme, wsa ist denn hier los</p>
+            <Link href="/tasks">Zu den Tasks</Link>;
+        </div>
+    )
+}
