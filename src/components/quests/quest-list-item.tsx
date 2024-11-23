72b1dcb353459d419c4a20a53d4ab7a19a32304e
Auxburger <darius.augsburger@redpoint.teseon.com>
2024-11-23 16:02:28 +0100

    add quest list

---
+++ src/components/quests/quest-list-item.tsx
@@ -0,0 +1,35 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Quest } from "@/lib/model/quest";
+import { UserCircle } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+
+export default function QuestListItem({ quest }: { quest: Quest }) {
+  return (
+    <Card className="w-full hover:bg-slate-100 transition-colors">
+      <div className="flex items-center pl-6 gap-2">
+        <Avatar>
+          <AvatarImage src="https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/428880/capsule_616x353.jpg?t=1667030814" />
+          <AvatarFallback>
+            <UserCircle className="text-slate-700" />
+          </AvatarFallback>
+        </Avatar>
+        <CardHeader>
+          <CardTitle>{quest.title}</CardTitle>
+          <CardDescription>{quest.description}</CardDescription>
+        </CardHeader>
+      </div>
+      <CardContent className="text-sm text-slate-600">
+        <p>
+          {quest.rewards.experience_points} <b>XP</b>
+        </p>
+        <p>🏁 Ends on {new Date(quest.time_window.end_time).toDateString()}</p>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/components/quests/quest-list.tsx
@@ -0,0 +1,88 @@
+import { Quest } from "../../lib/model/quest";
+import QuestListItem from "./quest-list-item";
+
+export default function QuestList() {
+  const quests = [
+    {
+      title: "Defeat the Dragon",
+      description: "Slay the dragon terrorizing the village.",
+      location: "Dragon's Lair",
+      time_window: {
+        start_time: "2023-01-15T09:00:00Z",
+        end_time: "2023-01-15T17:00:00Z",
+      },
+      rewards: {
+        experience_points: 500,
+        items: ["sword_of_truth", "shield_of_valor"],
+      },
+      quest_id: "f47ac10b-58cc-4372-a567-0e02b2c3d479",
+      creator_wallet: "0xabcdefabcdefabcdefabcdefabcdefabcdef",
+      participant_wallet: "0x1234567890abcdef1234567890abcdef12345678",
+      status: "available",
+      created_at: "2023-01-10T10:00:00Z",
+      updated_at: "2023-01-12T12:00:00Z",
+    },
+    {
+      quest_id: 2,
+      title: "Quest 2",
+      rewards: {
+        experience_points: 200,
+        items: ["item3", "item4"],
+      },
+      time_window: {
+        start_time: "2023-01-15T09:00:00Z",
+        end_time: "2023-01-15T17:00:00Z",
+      },
+      description: "Description 2",
+    },
+    {
+      quest_id: 3,
+      title: "Quest 3",
+      rewards: {
+        experience_points: 300,
+        items: ["item5", "item6"],
+      },
+      time_window: {
+        start_time: "2023-01-15T09:00:00Z",
+        end_time: "2023-01-15T17:00:00Z",
+      },
+      description: "Description 3",
+    },
+    {
+      quest_id: 4,
+      title: "Quest 4",
+      rewards: {
+        experience_points: 300,
+        items: ["item5", "item6"],
+      },
+      time_window: {
+        start_time: "2023-01-15T09:00:00Z",
+        end_time: "2023-01-15T17:00:00Z",
+      },
+      description: "Description 4",
+    },
+    {
+      quest_id: 5,
+      title: "Quest 5",
+      rewards: {
+        experience_points: 300,
+        items: ["item5", "item6"],
+      },
+      time_window: {
+        start_time: "2023-01-15T09:00:00Z",
+        end_time: "2023-01-15T17:00:00Z",
+      },
+      description: "Description 5",
+    },
+  ] as any as Quest[];
+  return (
+    <div className="flex min-h-screen w-full flex-col items-start p-4">
+      <h1 className="text-4xl font-bold text-primary">Quests</h1>
+      <div className="flex gap-2 flex-col w-full py-4">
+        {quests.map((quest) => (
+          <QuestListItem key={quest.quest_id} quest={quest} />
+        ))}
+      </div>
+    </div>
+  );
+}
