a21397d72cce4b2e1803fe577248ae2d0a2c7c15
Tabea Klein <tabea.klein@redpoint.teseon.com>
2024-11-23 20:53:05 +0100

    Add edit page and fix styling of profile page

--- src/app/game/profile/page.tsx
+++ src/app/game/profile/page.tsx
@@ -1,8 +1,9 @@
+import Avatar from "@/assets/avatar/male.png";
+import Background from "@/assets/backgrounds/501501-munich.png";
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
 import { UserRoundPen } from "lucide-react";
-import Background from "@/assets/backgrounds/501501-munich.png";
-import Avatar from "@/assets/avatar/male.png";
+import Link from "next/link";
 
 export default function Profile() {
   const lvl = "3";
@@ -11,8 +12,8 @@ export default function Profile() {
   return (
     <div className="flex flex-col h-screen">
       <div
-        className="w-full h-full object-cover"
-        style={{ flexBasis: "200px" }}
+        className="w-full object-cover overflow-hidden"
+        style={{ flexBasis: "225px" }}
       >
         <img
           src={Background.src}
@@ -20,7 +21,10 @@ export default function Profile() {
           className="w-full h-full object-cover blur-sm"
         />
       </div>
-      <div className="absolute left-1/2 top-1/3 transform -translate-x-1/2 -translate-y-1/2 w-40 h-40">
+      <div
+        className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-40 h-40"
+        style={{ marginTop: "180px" }}
+      >
         <img
           src={Avatar.src}
           alt="Avatar"
@@ -33,13 +37,15 @@ export default function Profile() {
       >
         <div className="flex">
           <p className="text-black pr-4">OG Hero</p>
-          <Button className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full w-10 h-6">
-            <UserRoundPen className="w-4 h-4" />
-          </Button>
+          <Link href="/game/profile/edit">
+            <Button className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full w-10 h-6">
+              <UserRoundPen className="w-4 h-4" />
+            </Button>
+          </Link>
         </div>
 
         <div className="w-3/4 flex">
-          <p className="text-black pr-4">Lvl. 5</p>
+          <p className="text-black pr-4">{lvl}</p>
           <Progress
             className=" self-center static"
             indicatorColor="bg-orange-700"
@@ -47,7 +53,7 @@ export default function Profile() {
             style={{ width: "75%", background: "lightgray" }}
           ></Progress>
         </div>
-        <p className="text-black self-start pl-4">Items</p>
+        <p className="text-black font-bold self-start pl-4 pt-6">Items</p>
         <div className="">
           {/* Grid container */}
           <div className="w-100% grid grid-cols-4 gap-4">
