70e2129e0830b6f0e9e868fb8c9e9e694f410fbe
Auxburger <darius.augsburger@redpoint.teseon.com>
2024-11-23 11:37:10 +0100

    wallet disconnection

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import dynamic from "next/dynamic";
 import ThemeSwitcher from "./themeSwitcher";
+import { WalletDisconnectButton, WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 
-const WalletMultiButtonDynamic = dynamic(
-  async () =>
-    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
-  { ssr: false }
-);
 
 const Header = () => {
   return (
@@ -17,7 +13,8 @@ const Header = () => {
         <code className="font-mono font-bold">src/app/page.tsx</code>
       </p>
       <div className="flex pt-4 lg:pt-0 w-full items-end justify-center gap-4 lg:static lg:size-auto lg:bg-none">
-        <WalletMultiButtonDynamic />
+        <WalletMultiButton />
+        <WalletDisconnectButton />
         <ThemeSwitcher />
       </div>
     </div>
